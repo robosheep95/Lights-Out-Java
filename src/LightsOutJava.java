6c98ccc4ecbf0fcc24cf6fc38a34e075e2625fb4
Taylor Scafe <scafetaylor@gmail.com>
2018-01-12 13:52:00 -0600

    formatted Project

--- src/LightsOutJava.java
+++ src/LightsOutJava.java
@@ -1,3 +1,4 @@
+
 /*
  * Taylor Scafe
  * 3/1/2016
@@ -18,95 +19,97 @@ public class LightsOutJava extends JFrame {
 	private Container window;
 	private JButton[][] buttonList;
 	public volatile boolean gameDone = false;
-	
-	public LightsOutJava(int xsize, int ysize){//Takes in row and column dimentions
+
+	public LightsOutJava(int xsize, int ysize) {// Takes in row and column dimentions
 		super("Lights Out");
 		buttonList = new JButton[xsize][ysize];
 		window = getContentPane();
-		window.setLayout(new GridLayout(0,ysize));
-		for(int i = 0;i < buttonList.length;i++){//Generates tiles and adds listeners to each button.
-		for(int j = 0;j < buttonList[i].length;j++){
-			buttonList[i][j] = new JButton("");
-			buttonList[i][j].setBackground(Color.white);
-			buttonList[i][j].addActionListener(new ButtonPress());
-			window.add(buttonList[i][j]);
+		window.setLayout(new GridLayout(0, ysize));
+		for (int i = 0; i < buttonList.length; i++) {// Generates tiles and adds listeners to each button.
+			for (int j = 0; j < buttonList[i].length; j++) {
+				buttonList[i][j] = new JButton("");
+				buttonList[i][j].setBackground(Color.white);
+				buttonList[i][j].addActionListener(new ButtonPress());
+				window.add(buttonList[i][j]);
+			}
 		}
-		}
-		setSize(ysize*100,xsize*100);//Sets window size based on size of grid
-		
-		Random ranGen = new Random();//Generates a random boolean to randomize the table but ensure that the puzzle is solvable
-		for (JButton[] buttonrow : buttonList){
-		for (JButton button : buttonrow){
-			boolean temp = ranGen.nextBoolean();
-		if (temp){
-			button.doClick();
-		}
-		}
+		setSize(ysize * 100, xsize * 100);// Sets window size based on size of grid
+
+		Random ranGen = new Random();// Generates a random boolean to randomize the table but ensure that the puzzle
+										// is solvable
+		for (JButton[] buttonrow : buttonList) {
+			for (JButton button : buttonrow) {
+				boolean temp = ranGen.nextBoolean();
+				if (temp) {
+					button.doClick();
+				}
+			}
 		}
 		window.repaint();
 		setVisible(true);
 	}
-	private class ButtonPress implements ActionListener{
+
+	private class ButtonPress implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent ea) {
 			boolean done = false;
 			int x = 0;
 			int y = 0;
-			for (int i = 0; i < buttonList.length; i++){
-				for (int j = 0; j < buttonList[i].length;j++){
-					if (((JButton) ea.getSource()).equals(buttonList[i][j])){
+			for (int i = 0; i < buttonList.length; i++) {
+				for (int j = 0; j < buttonList[i].length; j++) {
+					if (((JButton) ea.getSource()).equals(buttonList[i][j])) {
 						done = true;
-						//System.out.println(i+" | "+j);
+						// System.out.println(i+" | "+j);
 						x = i;
 						y = j;
 						break;
 					}
 				}
-				if (done){
+				if (done) {
 					break;
 				}
-			}			
+			}
 			switchColor(buttonList[x][y]);
-			if (x>0){//Check if there is a space available to the left of the selected tile
-				switchColor(buttonList[x-1][y]);
+			if (x > 0) {// Check if there is a space available to the left of the selected tile
+				switchColor(buttonList[x - 1][y]);
 			}
-			if(x<buttonList.length-1){//Check if there is a space available to the right of the selected tile
-				switchColor(buttonList[x+1][y]);
+			if (x < buttonList.length - 1) {// Check if there is a space available to the right of the selected tile
+				switchColor(buttonList[x + 1][y]);
 			}
-			if (y>0){//Check if there is a space available to the top of the selected tile
-				switchColor(buttonList[x][y-1]);
+			if (y > 0) {// Check if there is a space available to the top of the selected tile
+				switchColor(buttonList[x][y - 1]);
 			}
-			if(y<buttonList[x].length-1){//Check if there is a space available to the bottom of the selected tile
-				switchColor(buttonList[x][y+1]);
+			if (y < buttonList[x].length - 1) {// Check if there is a space available to the bottom of the selected tile
+				switchColor(buttonList[x][y + 1]);
 			}
-			if(checkWin()){//Checks for a winner and executes freeze and toggles gameDone variable
-				for (JButton[] buttonrow : buttonList){
-				for (JButton button : buttonrow){
-					button.setBackground(Color.green);
-					button.setEnabled(false);
-					gameDone = true;
+			if (checkWin()) {// Checks for a winner and executes freeze and toggles gameDone variable
+				for (JButton[] buttonrow : buttonList) {
+					for (JButton button : buttonrow) {
+						button.setBackground(Color.green);
+						button.setEnabled(false);
+						gameDone = true;
+					}
 				}
-				}
 			}
-			
-			
+
 		}
-		private boolean checkWin() {//Checks for all tiles to be black
+
+		private boolean checkWin() {// Checks for all tiles to be black
 			// TODO Auto-generated method stub
-			for (JButton[] buttonrow : buttonList){
-			for (JButton button : buttonrow){
-				if (button.getBackground() == Color.white){
-					return false;
+			for (JButton[] buttonrow : buttonList) {
+				for (JButton button : buttonrow) {
+					if (button.getBackground() == Color.white) {
+						return false;
+					}
 				}
-			}
 			}
 			return true;
 		}
-		public void switchColor(JButton button){//Used to switch color from black to white or vice versa.
-			if (button.getBackground().equals(Color.black)){
+
+		public void switchColor(JButton button) {// Used to switch color from black to white or vice versa.
+			if (button.getBackground().equals(Color.black)) {
 				button.setBackground(Color.white);
-			}
-			else{
+			} else {
 				button.setBackground(Color.black);
 			}
 		}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 /*
  * Taylor Scafe
  * 3/1/2016
@@ -12,45 +13,43 @@ public class Main {
 	public static int ysize;
 	public static boolean notDone = true;
 
-	public static void main(String[] args){
-		do{
+	public static void main(String[] args) {
+		do {
 			setXandY();
-			if(!notDone){
+			if (!notDone) {
 				JOptionPane.showMessageDialog(null, "Thanks for Playing!");
 				notDone = false;
-			}
-			else if (xsize==0 || ysize==0){
+			} else if (xsize == 0 || ysize == 0) {
 				JOptionPane.showMessageDialog(null, "Number of rows or columns cannot be 0");
 				setXandY();
-			}
-			else{
-				LightsOutJava game = new LightsOutJava(xsize,ysize);
+			} else {
+				LightsOutJava game = new LightsOutJava(xsize, ysize);
 				game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				while(!game.gameDone){
+				while (!game.gameDone) {
 				}
-				if(JOptionPane.showConfirmDialog(null, "You Won!\nPlay Again?", "Lights Out",  JOptionPane.YES_NO_OPTION)!=0){
+				if (JOptionPane.showConfirmDialog(null, "You Won!\nPlay Again?", "Lights Out",
+						JOptionPane.YES_NO_OPTION) != 0) {
 					JOptionPane.showMessageDialog(null, "Thanks for Playing!");
 					notDone = false;
 				}
 				game.dispose();
 			}
-		}while(notDone);
+		} while (notDone);
 
 	}
-	public static void setXandY(){//Creates dialog boxes to ask for row and column dimensions
+
+	public static void setXandY() {// Creates dialog boxes to ask for row and column dimensions
 		String xInput = null;
 		String yInput = null;
-		try{
+		try {
 			xInput = JOptionPane.showInputDialog("Enter the number of rows you want");
 			xsize = Integer.parseInt(xInput);
 			yInput = JOptionPane.showInputDialog("Enter the number of columns you want");
 			ysize = Integer.parseInt(yInput);
-		}
-		catch(NumberFormatException e){
-			if (xInput == null || yInput== null){
+		} catch (NumberFormatException e) {
+			if (xInput == null || yInput == null) {
 				notDone = false;
-			}
-			else{
+			} else {
 				JOptionPane.showMessageDialog(null, "Please only use integers");
 				setXandY();
 			}
